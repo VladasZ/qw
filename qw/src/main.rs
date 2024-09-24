b548aac3487dda71f00a7cce544633fb126f7031
Vladas <146100@gmail.com>
2024-09-24 21:48:44 +0300

    first args

--- qw/src/main.rs
+++ qw/src/main.rs
@@ -1,3 +1,14 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+enum Args {
+    New { name: String },
+}
+
 fn main() {
+    let args = Args::from_args();
+
+    dbg!(&args);
+
     dbg!("A");
 }
